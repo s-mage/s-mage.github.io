642487942ce4fb577019c205ef7021b80fa540cb
Sergey Smagin <smaginsergey1310@gmail.com>
2015-10-16 18:39:25 +0300

    Change Pavel's nickname to mine

--- javascripts/force-https.js
+++ javascripts/force-https.js
@@ -1,4 +1,4 @@
-var host = 'schfkt.github.io';
+var host = 's-mage.github.io';
 if (window.location.host === host && window.location.protocol !== 'https:') {
   window.location.protocol = 'https';
 }
